30197fce4b31e6555ce65aac8e76015ca005bf15
Adan Galicia <ansg75@gmail.com>
2021-11-23 15:42:14 -0600

    First commit

---
+++ app/src/main/java/com/example/fintecha75/LoginActivityClient.kt
@@ -0,0 +1,11 @@
+package com.example.fintecha75
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivityClient : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_client)
+    }
+}
