8ec42c07b6b13903a23c0e920fee74e06dd6ec70
Adan Galicia <ansg75@gmail.com>
2021-11-24 18:39:14 -0600

    Starting with SingUpActivityMarket.kt

--- app/src/main/java/com/example/fintecha75/SingUpActivityClient.kt
+++ app/src/main/java/com/example/fintecha75/SingUpActivityClient.kt
@@ -3,9 +3,9 @@ package com.example.fintecha75
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class StartActivity : AppCompatActivity() {
+class SingUpActivityClient : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+        setContentView(R.layout.activity_sing_up_client)
     }
 }
