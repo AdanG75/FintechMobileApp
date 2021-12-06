e86387c0829a58459e5e03f3402386da0912a826
Adan Galicia <ansg75@gmail.com>
2021-12-06 17:31:51 -0600

    Doing activity_movements_market.xml

---
+++ app/src/main/java/com/example/fintecha75/MovementsMarketActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fintecha75
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovementsMarketActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movements_market)
+    }
+}
