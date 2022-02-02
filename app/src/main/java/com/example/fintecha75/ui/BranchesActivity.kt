644c8b43853f110e351ee1d1d4fed74a67e137ce
Adan Galicia <ansg75@gmail.com>
2022-02-01 22:58:50 -0600

    Fixed issue Class not found exception: Didn't find class on path dexpath list

--- app/src/main/java/com/example/fintecha75/ui/BranchesActivity.kt
+++ app/src/main/java/com/example/fintecha75/ui/BranchesActivity.kt
@@ -2,6 +2,7 @@ package com.example.fintecha75.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fintecha75.R
 
 class BranchesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
