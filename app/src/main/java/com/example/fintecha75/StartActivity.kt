0387ecc07a170e5057e927648a0255e708571cb6
Adan Galicia <ansg75@gmail.com>
2022-02-04 23:21:59 -0600

    Started with StartActivity.kt

--- app/src/main/java/com/example/fintecha75/StartActivity.kt
+++ app/src/main/java/com/example/fintecha75/StartActivity.kt
@@ -1,11 +1,30 @@
 package com.example.fintecha75
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        val clientButton = findViewById<ImageButton>(R.id.ivStartActivityPersonButton)
+        val marketButton = findViewById<ImageButton>(R.id.ivStartActivityMarketButton)
+
+        clientButton.setOnClickListener {
+            val intent = Intent(this@StartActivity, LoginActivityClient::class.java)
+            startActivity(intent)
+        }
+
+        marketButton.setOnClickListener {
+            val intent = Intent(this@StartActivity, LoginActivityMarket::class.java)
+            startActivity(intent)
+        }
+
+
     }
+
 }
